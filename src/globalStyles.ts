8b848367709c4da188babd51ff5b810cba17bcfe
Matheus Victor <matheusvictor2000@gmail.com>
2022-12-29 11:17:09 -0300

    adicionando a fonte e criando o global style

---
+++ src/globalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: 'Roboto', sans-serif;
+        background-color: #F1F1F1;
+        
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-weight: 500;
+    }
+
+`
