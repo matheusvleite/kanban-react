5a1172691be609078015a9f878e72aa05cd7a0a7
Matheus Victor <matheusvictor2000@gmail.com>
2022-12-30 14:21:17 -0300

    função adicionar e remover card tarefa fazendo

--- src/Components/Fazendo/index.tsx
+++ src/Components/Fazendo/index.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ITarefas } from "../../interfaces/tarefas";
 import { Card } from "../Card"
 import { ModalTarefa } from "../ModalTarefa";
 
 export const Fazendo = () => {
+    const getTarefa: ITarefas[] = JSON.parse(localStorage.getItem('tarefasFazendo')!) || []
+
     const [open, setOpen] = useState(false);
+    const [tarefa, setTarefa] = useState('');
+    const [tarefas, setTarefas] = useState<ITarefas[]>(getTarefa);
+
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    useEffect(() => {
+        localStorage.setItem('tarefasFazendo', JSON.stringify(tarefas));
+    }, [tarefas])
     
     return (
         <>
@@ -13,10 +23,16 @@ export const Fazendo = () => {
                 titulo="Fazendo"
                 cor="#06BFF0"
                 handleOpen={handleOpen}
+                tarefas={tarefas}
+                setTarefas={setTarefas}
             />
             <ModalTarefa
                 handleClose={handleClose}
                 open={open}
+                setTarefa={setTarefa}
+                tarefa={tarefa}
+                setTarefas={setTarefas}
+                tarefas={tarefas}
             />
         </>
     )
